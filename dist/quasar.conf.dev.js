8974e8b5805d6513e982f5d90f72ea274f4d6b30
@Atemndobs <atemndobs@yahoo.com>
2020-08-27 19:26:51 +0200

    add icons

--- dist/quasar.conf.dev.js
+++ dist/quasar.conf.dev.js
@@ -109,7 +109,7 @@ module.exports = configure(function ()
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#212121',
         icons: [{
           src: 'icons/icon-128x128.png',
           sizes: '128x128',
--- quasar.conf.js
+++ quasar.conf.js
@@ -128,7 +128,7 @@ module.exports = configure(function (/* ctx */) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#212121',
         icons: [
           {
             src: 'icons/icon-128x128.png',
